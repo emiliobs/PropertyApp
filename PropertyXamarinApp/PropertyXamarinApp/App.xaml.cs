a9036999576831bcbfa377de1c0d2ae07d17bfe8
Emilio Barrea <barrera_emilio@yahoo.es>
2020-06-28 13:01:17 +0100

    Embedded Fonts in Xamarin Forms

--- PropertyXamarinApp/PropertyXamarinApp/App.xaml.cs
+++ PropertyXamarinApp/PropertyXamarinApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace PropertyXamarinApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
